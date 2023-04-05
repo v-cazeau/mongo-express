959ede5005a5e1e41e56d050c9f6a9453b41664b
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-04-05 14:44:46 -0400

    ADDED delete and update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getAllPlants, addPlant } from "./src/plants.js";
+import { getAllPlants, addPlant, deletePlant, updatePlant } from "./src/plants.js";
 
 const PORT = 3007; 
 const app = express(); 
@@ -8,8 +8,14 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
+app.get("/",(req,res) => {
+    res.send (`I am gRoot.`);
+});
+
 app.get("/plants" , getAllPlants); //api points/routes will go here...
 app.post("/plants", addPlant); 
+app.delete("/plants/:docID",deletePlant);
+app.patch("/plants/:docId",updatePlant); 
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}...`);
--- src/plants.js
+++ src/plants.js
@@ -1,13 +1,38 @@
 import { db } from "./connectDb.js"; 
+import { ObjectId } from "mongodb";
+
 const coll = db.collection("plants");
 
+//CRUD: GET 
 export async function getAllPlants(req,res) {
     const plants = await coll.find({}).toArray();
     res.send(plants); 
 }
 
+//CRUD: POST
 export async function addPlant(req,res) {
     const newPlant = req.body; 
     await coll.insertOne(newPlant); 
-    res.status(201).send("added plant");
+    res.status(201).send(" new plant added");
+}
+
+//CRUD: DELETE 
+export async function deletePlant(req,res){
+    const docId = {"_id": new ObjectId(req.params.docId)}
+    await coll.deleteOne(docId);
+    res.status(201).send({message: "Plants has been sadly deleted."})
+}
+
+//CRUD: UPDATE: target the current exisitng content and update it with an updated content
+
+export async function updatePlant(req, res) {
+    const docID = {"_id": new ObjectId (req.params.docId )};
+    const updatePlant = req.body; 
+
+    await coll.findOneAndUpdate (
+        {"_id": docID},
+        { $set: { updatePlant } }
+    );
+    res.status(201).send({ message: "plant has been updated"})
+
 }
